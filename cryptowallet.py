0fefca6b6c948a6e518984ede0bfedf6d3a7ed3a
marom <m.sverdlov@student.tue.nl>
2023-06-06 00:42:34 +0200

    changed mal.py text so that it actuallyu gives the right address

---
+++ cryptowallet.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def get_balance(wallet_address):
+ 
+
+
+    # API endpoint URL for retrieving wallet balance from blockchain.com API
+    balance_url = "https://live.blockcypher.com/btc-testnet/address/" + wallet_address
+
+
+    # Make API request to retrieve wallet balance
+    response = requests.get(balance_url)
+
+    response_text = response.text
+    index =  response_text.find("balance of")
+    balance = float(response_text[index+11:index + 18])
+    return balance
+
+       # Wallet address
+   
+address = "tb1qud9u85mcjcwndgwjqgcw69neah9z22kp7uw9wv" #Attacker crypto wallet address
+
+get_balance(address)
