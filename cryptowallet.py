bb70f5d1d9ba018eb447f3950cef5f00422e7b68
yusefahmd <104899745+yusefahmd@users.noreply.github.com>
2023-06-07 15:44:15 +0200

    Fixed web-scraping parsing for the last time

--- cryptowallet.py
+++ cryptowallet.py
@@ -1,10 +1,12 @@
 import requests
+import re
 
 
 def get_balance(wallet_address):
  
     # API endpoint URL for retrieving wallet balance from blockchain.com API
     balance_url = "https://live.blockcypher.com/btc-testnet/address/" + wallet_address
+    decimal_pattern = r'\d+\.\d+'
 
 
     # Make API request to retrieve wallet balance
@@ -13,8 +15,10 @@ def get_balance(wallet_address):
     response_text = response.text
     index =  response_text.find("balance of")
     balance_str = response_text[index+11:index + 20]
+    match = re.search(decimal_pattern, balance_str)
+    balance = float(match.group())
     balance = [float(s) for s in balance_str.split() if s.isdigit()]
-    return balance[0]
+    return balance
 
        # Wallet address
    
